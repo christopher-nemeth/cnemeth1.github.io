ee741651d1584f5411e092516fefd29c2de0fdff
Christopher Nemeth <cnemeth1@gmail.com>
2018-08-22 10:19:17 -0600

    working on resume modal

--- js/index.js
+++ js/index.js
@@ -34,3 +34,17 @@ function revealForm(event){
     } // End if
   });
 });
+
+var modal = document.getElementById('myModal')
+var resumeLink = document.querySelector('#resume')
+var captionText = document.getElementById('caption')
+
+resumeLink.onClick = function () {
+    modal.style.display = 'flex'
+}
+
+var span = document.getElementsByClassName('close')[0];
+
+span.onClick = function() {
+  modal.style.display = 'none'
+}
